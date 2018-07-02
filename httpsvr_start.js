c53d4e2c805fc631d00d549306fcdca52f50b6b6
aShinjiroUrata <shinjiro.urata@access-company.com>
2018-07-02 19:02:12 +0900

    deleted unnecessary files

--- httpsvr_start.js
+++
@@ -1,7 +0,0 @@
-var sys = require('util')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-//exec("http-server . -p 8081 -d false", puts);
-exec("http-server . -p 8081 -d false > log.txt 2>&1 &", puts);
-
-
